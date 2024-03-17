7886939e6a6b1c69f8d3f75b9d7addd8c3ad127e
bluej1996 <bluej1996@gmail.com>
2024-03-17 17:11:23 +0900

    eslint 추가

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    // no-unused-vars 규칙을 무시하고 싶을 때:
+    "no-unused-vars": "off",
+    "react/prop-types": "off",
+  },
+};
